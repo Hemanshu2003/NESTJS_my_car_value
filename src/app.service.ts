ecaf6789efd566ac28bb3277f0decd87992d05b2
Hemanshu Waghmare <113788603+Hemanshu2003@users.noreply.github.com>
2023-05-28 17:10:40 +0530

    UPDATE

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '<h1 style="color:blue;">WELCOME TO My Car Value API</h1><p>Created By : @hemanshu_waghmare</p>';
   }
 }
